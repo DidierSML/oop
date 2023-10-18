fecd1a9bd230f6a3250e05c18cf8a2597d92955a
DidierSML <didiersmaldonad@gmail.com>
2023-10-18 15:02:20 -0500

    ArithmeticOperations

---
+++ src/main/java/org/example/algoritms/ArithmeticOperations.java
@@ -0,0 +1,35 @@
+package org.example.algoritms;
+
+import java.util.Scanner;
+
+/*
+    Diseñe un algoritmo que permita calcular las operaciones
+    aritméticas básicas (+ - * /) ingresando 2 valores numericos
+    por teclado.
+ */
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese los 2 números sobre los cuales quiere realizar calculos aritmeticos:");
+
+        //Datos de entrada (input)
+        int primerNumero = input.nextInt();
+        int segundoNumero = input.nextInt();
+
+        //Acciones (suma - resta - multiplicación- division)
+        int suma = (primerNumero + segundoNumero);
+        int resta = (primerNumero - segundoNumero);
+        int multiplicación = (primerNumero * segundoNumero);
+        int division = (primerNumero / segundoNumero);
+
+        //Salida (Output)
+        System.out.println("Resultados: \nLa suma de los numeros es: " + suma);
+        System.out.println("La resta de los numeros es: " + resta);
+        System.out.println("La multiplicacion de los numeros es: " + multiplicación);
+        System.out.println("La division de los numeros es: " + division);
+    }
+}
