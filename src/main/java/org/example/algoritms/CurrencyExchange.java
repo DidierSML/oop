f515b28dc666ffcaaf0999eee080ee65f79be472
DidierSML <didiersmaldonad@gmail.com>
2023-10-19 14:30:28 -0500

    Currency Exchange & IVA Exercise

---
+++ src/main/java/org/example/algoritms/CurrencyExchange.java
@@ -0,0 +1,41 @@
+package org.example.algoritms;
+
+import java.util.Scanner;
+
+/*
+    Realizar un algoritmo de tipo de cambio de moneda
+    sabiendo que 1 dolar es igual a 3,34 soles peruanos.
+
+    input = dolares;
+    constant = soles (3.34);
+    process = dolares * soles;
+
+ */
+public class CurrencyExchange {
+
+     public static void main(String[] args) {
+
+         Scanner input = new Scanner(System.in);
+
+         System.out.println("Ingrese la cantidad de USD a convertir en Soles:");
+         //Input(datos de entrada)
+         double dolaresEntrada = input.nextDouble();
+
+         System.out.println("Ingrese la cantidad de Soles a convertir en Dolares:");
+         //Input(datos de entrada)
+         double solesEntrada = input.nextDouble();
+
+         //Constant(constante) Equivalencia 1USD 3.34 Soles
+         double solesConstante = 3.34;
+
+         //Process(proceso)
+         double conversionASoles = (dolaresEntrada * solesConstante);
+
+         //Process(proceso)
+         double conversionADolares = (solesEntrada / solesConstante);
+
+         //Output(salida)
+         System.out.println(dolaresEntrada + " USD equivale(n) a " + conversionASoles + " Soles");
+         System.out.println(solesEntrada + " Soles equivale(n) a " + conversionADolares + " USD");
+     }
+}
---
+++ src/main/java/org/example/algoritms/Iva.java
@@ -0,0 +1,49 @@
+package org.example.algoritms;
+
+import java.util.Scanner;
+
+/*
+    Diseñe un algoritmo que lea por consola el valor de una factura,
+    en este caso aplique un IVA del 18%.
+
+    input: valor de la factura
+    process: valor de la factura * 1.18
+    output: valor de la factura más iva
+ */
+public class Iva {
+
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese el valor de la factura para añadirle el IVA");
+        //input(datos de entrada)
+        double factura = input.nextDouble();
+
+        //Constant(constante)
+        double iva = 1.18;
+
+        //Process(proceso)
+        double totalConIva = (factura * iva);
+
+        //Output(output)
+        System.out.println("La Factura con el IVA incluido tiene un valor total de: " + totalConIva);
+
+
+    }
+}
+
+ /*
+    El calculo suma inmediatamente el iva a la factura, aunque tambien pudimos
+    hacerlo así:
+
+        double iva = 0,18;
+        double factura = input.nextDouble();
+
+        double valorIvaASumar = (factura * iva);
+        double totalConIva = (factura + valorIvaASumar)
+
+        sout(valorIvaASumar);
+        sout(totalConIva);
+ */
