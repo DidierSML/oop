6593ee227cd9438a25d8a203c5ed82ac9ba2e7c0
DidierSML <didiersmaldonad@gmail.com>
2023-10-27 16:02:10 -0500

    Serie From 0 to X number and printing of the sum of its even numbers

---
+++ src/main/java/org/example/alghoritms/whileExercises/Serie.java
@@ -0,0 +1,30 @@
+package org.example.alghoritms.whileExercises;
+
+import java.util.Scanner;
+
+/*
+    Diseñe un algoritmo que permita ingresar el ultimo numero de una serie.
+    Ej: mostrado a continuación :
+    2, 4, 6 , 8 , 10 ,……..;
+    Mostrar la suma total de la serie completa en la pantalla.
+ */
+public class Serie {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese el ultimo numero de la serie: ");
+        int numeroFinal = input.nextInt();
+
+        int serie = 0;
+        int suma = 0;
+
+        while (serie <= numeroFinal){
+            suma = suma + serie;
+            serie = serie + 2;
+        }
+
+        System.out.println("la suma total de los pares desde 0 hasta "+ numeroFinal + " es: " + suma);
+    }
+}
