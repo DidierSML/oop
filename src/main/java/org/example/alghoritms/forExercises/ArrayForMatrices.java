3cad578229f2710b9847b8a42a30ed436aed0069
DidierSML <didiersmaldonad@gmail.com>
2023-11-08 16:17:26 -0500

    Array For Matrices

--- src/main/java/org/example/alghoritms/forExercises/ArrayForMatrices.java
+++ src/main/java/org/example/alghoritms/forExercises/ArrayForMatrices.java
@@ -5,6 +5,8 @@ package org.example.alghoritms.forExercises;
     Siempre! Siempre! se declara primero la (fila) y luego la (columna).
  */
 
+import java.util.Scanner;
+
 //Recorriendo una matriz
 public class ArrayForMatrices {
 
@@ -23,3 +25,79 @@ public class ArrayForMatrices {
     }
 
 }
+
+//Aisgnando valores y recorriendo la matriz
+class AsignandoValoresMatriz {
+
+    public static void main(String[] args) {
+
+        int [][] matriz = {
+                {1,2,3,4},
+                {5,6,7,8},
+        };
+
+        for (int fila = 0; fila < matriz.length; fila++){
+            for (int columna = 0; columna < matriz[fila].length;columna++){
+                int valor = (matriz[fila][columna]);
+                System.out.println("Fila: " + fila + " Columna: " + columna + " = " + valor);
+            }
+        }
+
+
+    }
+
+}
+
+//Asignando valores tamaño de la matriz y valores por teclado
+class AsignacionPorTeclado {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de filas:");
+        int totalFilas = input.nextInt();
+
+        System.out.println("Ingrese la cantidad de columnas:");
+        int totalColumnas = input.nextInt();
+
+        int [][] matriz = new int[totalFilas][totalColumnas];
+
+        //Muestre los indices de la matriz
+        for (int fila = 0; fila < totalFilas; fila ++) {
+            for (int columna = 0; columna < totalColumnas; columna++) {
+                System.out.println("Fila: " + fila + " Columna: " + columna);
+
+            }
+        }
+
+        System.out.println("Ingrese los valores de la matriz:");
+        //Ingrese los valores de la matriz
+        for (int fila = 0; fila < totalFilas; fila++){//2
+            for (int columna = 0; columna < totalColumnas; columna++){//3
+                matriz [fila][columna] = input.nextInt();
+            }
+        }
+
+
+        //Imprima los valores de la matriz
+        for (int fila = 0; fila < totalFilas; fila++){
+            for (int columna = 0; columna < totalColumnas; columna++){
+                System.out.print(matriz [fila][columna] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+}
+
+/*
+             int [][] matriz = new int [2][2];
+
+             matriz [0][0]=1;
+             matriz [0][1]=2;
+             matriz [1][0]=3;
+             matriz [1][1]=4
+*/
