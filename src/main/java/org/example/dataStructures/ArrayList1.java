889ad3b7fd88256ad0c7c84235b61c0dcab7c67d
DidierSML <didiersmaldonad@gmail.com>
2023-11-30 18:14:04 -0500

    Using ListIterator in Collection

--- src/main/java/org/example/dataStructures/ArrayList1.java
+++ src/main/java/org/example/dataStructures/ArrayList1.java
@@ -3,6 +3,7 @@ package org.example.dataStructures;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * A continuación veremos como se pueden crear ArrayList tradicionales,
@@ -51,6 +52,48 @@ public class ArrayList1 {
         for (String imprimir:listaColores) {
             System.out.println(imprimir);
         }
+
+        //Creando un Objeto iterator para recorrer una Lista (Asignando la listaColores al objeto -iterator-)
+        ListIterator <String> iterator = listaColores.listIterator();
+
+        //Recorrido normal de una List usando Iterator
+        System.out.println("\n\nRecorrido con Iterator: ");
+        while (iterator.hasNext()){ //Mientras haya algo que iterar en sentido normal
+            System.out.println("Color: " + iterator.next()); //Imprima lo que hay en cada índice
+        }
+
+        //Recorrido a la inversa de una List usando Iterator
+        System.out.println("\n\nRecorrido con Iterator a la Inversa");
+        while (iterator.hasPrevious()){ //Mientras haya algo que iterar pero en sentido inverso
+            System.out.println("Color: " + iterator.previous());//Imprima lo que hay en cada índice a la inversa
+        }
+/*
+
+        Este ciclo While deberia funcionar, sin embargo, como anteriormente iteramos con hasNext
+        el progrma entiende que ya no hay nada que iterar. Por ende usaremos en lugar el ciclo (For)
+
+        //Removiendo un elemento mediante los Metodos de ListIterator
+        System.out.println("\n\nRecorrido con Iterator sin Amarillo");
+        while (iterator.hasNext()){ //Mientras haya algo que iterar
+            if (iterator.next().equalsIgnoreCase("Amarillo")){
+                iterator.remove();
+            }
+            System.out.println("Color: " + iterator.previous());//Imprima lo que hay en cada índice a la inversa
+        }
+
+ */
+        //Recorriendo la lista y removiendo según la condición
+        while (iterator.hasNext()){
+            if (iterator.next().equals("Amarillo")){
+                iterator.remove();
+            }
+        }
+
+        System.out.println("\n\nRecorrido con iterator removiendo el color Amarillo usando For");
+        for (ListIterator <String> iterator2 = listaColores.listIterator();iterator2.hasNext();){
+            System.out.println("Color: " + iterator2.next());
+        }
+
 
 
     }
