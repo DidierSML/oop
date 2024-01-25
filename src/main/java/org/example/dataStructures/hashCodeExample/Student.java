1d9d3da5d46351d8092e979d8b7298386ebad61f
DidierSML <didiersmaldonad@gmail.com>
2024-01-25 10:06:37 -0500

    HashCode implementation

--- src/main/java/org/example/dataStructures/hashCodeExample/Student.java
+++ src/main/java/org/example/dataStructures/hashCodeExample/Student.java
@@ -54,7 +54,7 @@ public class Student {
     //HashCode generado
     @Override
     public int hashCode() {
-        //Calculo hash interno que generará el codigo hash y que asignamos a la var: -hashCodeResult-
+        //Cálculo hash interno que generará el código hash y que asignamos a la variable: -hashCodeResult-
         int hashCodeResult = Objects.hash(id, name, email, telephone);
         //Impresión de todos los HashCode generados desde el método
         System.out.println("HashCode: " + hashCodeResult);
@@ -63,12 +63,47 @@ public class Student {
     }
 
     /*
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Student student = (Student) o;
-            return id == student.id && Objects.equals(name, student.name) && Objects.equals(email, student.email) && Objects.equals(telephone, student.telephone);
-        }
-     */
+        Generando el Método Equals podemos definir cuales serán los criterios para definir
+        si un objeto es igual a otro o no; en este caso los criterios serán sus atributos
+    */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;//Comparación si el Obj es igual al otro a nivel de memoria ram
+        if (o == null || getClass() != o.getClass()) return false; //Si el obj comparado es null devolvera false  y si no es la misma clase -false
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name) && Objects.equals(email, student.email) && Objects.equals(telephone, student.telephone);
+    }
+
+    //El método toString nos permitirá que la impresión de un Objeto sea personalizada o indique su contenido
+    //Y no que arroje un valor extraño y sin función alguna
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
 }
+
+/**
+ * Consideraciones Método Equals:
+ *
+ * El Método Equals debe de ser:
+ *  - Reflexivo: En el caso de una referencia "a", a.equals(a) deber de retornar -true
+ *  - Simétrico: Dada una referencia a y b, si a.equals(b) retorna true, entonces b.equals(a) debe de retornar true.
+ *  - Transitivo: Dadas las referencias a, b y c. sí a.equals(b) devuelve true, y b.equals(c) devuelve true, entonces a.equals(c) debe devolver true.
+ *  - Consistente: Dadas las referencias a y b, multiples invocaciones de a.equals(b) deben de retornar siempre true o bien siempre false.
+ *
+ *  Nota: El métod Hash emplea el método Equals internamente, ya que si se presenta colision
+ *        por 2 objetos diferentes con el mismo valor hash generado, este procedera a ubicarlos
+ *        luego de comparar igualdades en las linkeList internas de estas.
+ *
+ *        De igual manera, el método contains de toda Colección usa internamente el método equals
+ *        para verificar el contenido a buscar
+ *
+ *  Nota: Siempre se recomienda implementar el Método Equals y HashCode
+ *
+ */
--- src/main/java/org/example/dataStructures/hashCodeExample/StudentApplication.java
+++ src/main/java/org/example/dataStructures/hashCodeExample/StudentApplication.java
@@ -22,8 +22,27 @@ public class StudentApplication {
         students.put(student3,student3);
         students.put(student4,student4);
 
+        //Probando si ambos son objetos iguales o no.
+        Student test1 = new Student(10, "Elias", "elias@gmail.com", "9876543211");
+        Student test2 = new Student(10, "Elias", "elias@gmail.com", "9876543211");
+
+        //Impresión post comparación
+        System.out.println("El objeto " + test1 + " es igual al objeto " + test2 + " ? : " + test1.equals(test2) );
+
         //Imprimiendo HashCode student1
         //  System.out.println("hashcode de student1: " + student1.hashCode()); //Método hash proporcionado por la (C)Object
         //hashcode de student1: 950721611
     }
 }
+
+/*
+
+    HashCode: 950721611
+    HashCode: -1036347446
+    HashCode: 956356873
+    HashCode: -747272472
+
+    El objeto Student{id=10, name='Elias', email='elias@gmail.com', telephone='9876543211'} es igual al objeto Student{id=10, name='Elias', email='elias@gmail.com', telephone='9876543211'} ? : true
+
+
+ */
